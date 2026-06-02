f5eeb2aea381b2ec03646b39e9d443fc6e8c1d4f
hipermr9-spec <hipermr9@gmail.com>
2026-06-03 02:16:08 +0300

    v1.0.1

--- Frontend/Pages/Help.jsx
+++ Frontend/Pages/Help.jsx
@@ -2,14 +2,17 @@
 import React, { useEffect, useState } from "react"
 import "./App.css"
 
-return (
-    <div className="help-container">
-      <h1 className="help-title">منصة المهارات التعليمية</h1>
-      <p className="help-desc">مرحباً بك في صفحة المساعدة! إذا كنت تواجه أي مشكلة أو لديك أي استفسار، لا تترددي في دخول في صفحة مساعدة.</p>
-        <div className="help-links">
-            <a href="Help/Verify/requirements" className="btn-main">متطلبات التحقق</a>
-            <a href="Help/Verify/steps" className="btn-main">خطوات التحقق</a>
+
+export default function Help() {
+    return (
+        <div className="help-container">
+            <h1 className="help-title">منصة المهارات التعليمية</h1>
+            <p className="help-desc">مرحباً بك في صفحة المساعدة! إذا كنت تواجه أي مشكلة أو لديك أي استفسار، لا تترددي في دخول في صفحة مساعدة.</p>
+                <div className="help-links">
+                <a href="Help/Verify/requirements" className="btn-main">متطلبات التحقق</a>
+                <a href="Help/Verify/steps" className="btn-main">خطوات التحقق</a>
+            </div>
+            <p className="help-section">مزيد من مساعدة قريبا.</p>
         </div>
-        <p className="help-section">مزيد من مساعدة قريبا.</p>
-    </div>
-)
+    )
+}
