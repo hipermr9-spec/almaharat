96c22ef33a13e5906bbc51b9bdf6eb556094d767
hipermr9-spec <hipermr9@gmail.com>
2026-06-03 02:25:56 +0300

    v1.0.1

--- Frontend/App.jsx
+++ Frontend/App.jsx
@@ -142,7 +142,7 @@ export default function App() {
           <Route path="/Change/Password" element={<ChangePassword />} />
           <Route path="/port/users/user/:id/tokens/:token" element={<tokens />} />
           <Route path="/Help" element={<Help />} />
-          <Route path="/Help/Verify/requirments" element={<HelpVRR />} />
+          <Route path="/Help/Verify/requirements" element={<HelpVRR />} />
           <Route path="*" element={<Error404 />} />
         </Routes>
       </ServerChecker>
