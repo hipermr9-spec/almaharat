5c13787520938f7e5e25f77334f2f2b9c4e27994
hipermr9-spec <hipermr9@gmail.com>
2026-08-11 20:22:12 +0300

    v1.9.6 FIXED FIXED

--- Backend/Python/app.py
+++ Backend/Python/app.py
@@ -45,27 +45,16 @@ ALLOWED_ORIGINS = [
     "http://localhost:5173"
 ]
 
-CORS(app, resources={
-    r"/*": {
-        "origins": ALLOWED_ORIGINS,
-        "allow_headers": [
-            "Content-Type",
-            "Authorization",
-            "X-Requested-With",
-            "Accept",
-            "ngrok-skip-browser-warning",
-            "X-Admin-Token",
-            "X-Owner-Token"
-        ],
-        "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
-    }
-}, supports_credentials=True)
+CORS(app, origins=ALLOWED_ORIGINS, supports_credentials=True)
 
 @app.after_request
 def add_cors_headers(response):
     origin = request.headers.get("Origin")
-    if origin and origin in ALLOWED_ORIGINS:
-        response.headers["Access-Control-Allow-Origin"] = origin
+    if origin:
+        if origin in ALLOWED_ORIGINS:
+            response.headers["Access-Control-Allow-Origin"] = origin
+        else:
+            response.headers["Access-Control-Allow-Origin"] = ", ".join(ALLOWED_ORIGINS)
         response.headers["Access-Control-Allow-Credentials"] = "true"
         response.headers["Access-Control-Allow-Headers"] = ", ".join([
             "Content-Type",
